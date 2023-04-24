027cc5414c95a33d4f4b242762d464e43364b6d3
HFConsultant <62076556+HFConsultant@users.noreply.github.com>
2023-04-24 10:12:51 -0700

    Update customChat.py

--- customChat.py
+++ customChat.py
@@ -1,9 +1,7 @@
 import os
-
 os.environ['OPENAI_API_KEY'] = 'SECRET-KEY'
 
 # Import llama_index and indexing the documents stored in the data folder
-
 from llama_index import GPTSimpleVectorIndex, Document, SimpleDirectoryReader
 
 documents = SimpleDirectoryReader('data').load_data()
@@ -13,8 +11,7 @@ index = GPTSimpleVectorIndex(documents)
 index.save_to_disk('index.json')
 
 # Bonus (Not required in the code, but for optimisation)
-
 # Read the index.json later on for doing the indexation only once (#MoneySaving)
-
 index = GPTSimpleVectorIndex.load_from_disk('index.json')
+
 print(index.query("What's next for AI Research?"))
